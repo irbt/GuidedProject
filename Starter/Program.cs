24065c2b2c2199ac7c5bfc2155ba81de59584fc2
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2024-06-13 14:02:15 +0200

    https://learn.microsoft.com/fr-fr/training/modules/guided-project-arrays-iteration-selection/5-exercise-implement-selection-statements

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -6,7 +6,7 @@ Console.Clear();
 int currentAssignments = 5;
 string[] studentNames = new string[] { "sophia", "andrew", "emma", "logan" };
 int[] studentScores = new int[10];
-
+string currentStudentLetterGrade = "";
 
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
 int[] andrewScores = new int[] { 92, 89, 81, 86, 90 };
@@ -47,7 +47,60 @@ foreach (string name in studentNames)
 
     studentScore = (decimal)studentSum / currentAssignments;
 
-    Console.WriteLine($"{currentStudent}:\t\t{studentScore}\t?");
+    if (studentScore >= 97)
+    {
+        currentStudentLetterGrade = "A+";
+    }
+    else if (studentScore >= 93)
+    {
+        currentStudentLetterGrade = "A";
+    }
+    else if (studentScore >= 90)
+    {
+        currentStudentLetterGrade = "A-";
+    }
+    else if (studentScore >= 87)
+    {
+        currentStudentLetterGrade = "B+";
+    }
+    else if (studentScore >= 83)
+    {
+        currentStudentLetterGrade = "B";
+    }
+    else if (studentScore >= 80)
+    {
+        currentStudentLetterGrade = "B-";
+    }
+    else if (studentScore >= 77)
+    {
+        currentStudentLetterGrade = "C+";
+    }
+    else if (studentScore >= 73)
+    {
+        currentStudentLetterGrade = "C";
+    }
+    else if (studentScore >= 70)
+    {
+        currentStudentLetterGrade = "C-";
+    }
+    else if (studentScore >= 67)
+    {
+        currentStudentLetterGrade = "D-";
+    }
+    else if (studentScore >= 63)
+    {
+        currentStudentLetterGrade = "D";
+    }
+    else if (studentScore >= 60)
+    {
+        currentStudentLetterGrade = "D-";
+    }
+    else 
+    {
+        currentStudentLetterGrade = "F";
+    }
+
+    Console.WriteLine($"{currentStudent}:\t\t{studentScore}\t{currentStudentLetterGrade}");
 
 }
 
